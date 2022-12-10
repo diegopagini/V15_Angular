ef32f4cf3760e39f39cde23ca838d2a5ac4de446
Diego Pagini <diegopaginidev@gmail.com>
2022-12-10 09:33:05 +0100

    nested routes

---
+++ src/app/pages/admin-page/components/panel/panel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-panel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './panel.component.html',
+  styleUrls: ['./panel.component.scss']
+})
+export class PanelComponent {
+
+}
