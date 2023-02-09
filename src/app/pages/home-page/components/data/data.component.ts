8901fbe780c1aa66c85b5da8fd10d44d7ddc2771
Diego Pagini <diego.pagini@neoris.com>
2023-02-09 16:38:35 +0100

    nested routes

---
+++ src/app/pages/home-page/components/data/data.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-data',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './data.component.html',
+  styleUrls: ['./data.component.scss']
+})
+export class DataComponent {
+
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { Observable } from 'rxjs';
 import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
 
 @Component({
   standalone: true,
   selector: 'app-home-page',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
   providers: [PokemonService],
